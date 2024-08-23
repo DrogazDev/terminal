bfe89a8d04f95338a412c962cb2e412880e98ef7
DrogazDev <rootzeyla@gmail.com>
2024-08-24 01:02:58 +0200

    .

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+"use client"
+
+import React, { useState, useEffect } from 'react';
 
 type Command = {
     command: string;
@@ -8,44 +10,99 @@ type Command = {
 const Terminal: React.FC = () => {
     const [history, setHistory] = useState<Command[]>([]);
     const [input, setInput] = useState<string>('');
+    const [displayedResult, setDisplayedResult] = useState<string>('');
+    const [isTyping, setIsTyping] = useState<boolean>(false);
 
     const handleCommand = (command: string) => {
         const lowerCommand = command.toLowerCase();
         const output = executeCommand(lowerCommand);
-        setHistory([...history, {command, result: output}]);
+
+        setIsTyping(true);  // Start typing effect
+        setDisplayedResult('');  // Clear previous result display
+
+        const newCommand = { command: "/ " + command, result: output };
+        setHistory([...history, newCommand]);
+        typeWriterEffect(output);
+    };
+
+    const typeWriterEffect = (text: string) => {
+        let i = 0, type = () => i <= text.length ? (setDisplayedResult(text.slice(0, i++)), setTimeout(type, 25)) : setIsTyping(false);
+        type();
     };
 
     const executeCommand = (command: string): string => {
+
         const commands: { [key: string]: () => string } = {
-            help: () => "Available commands: help, about, projects",
             about: () => "I'm a passionate developer from the Netherlands skilled in Laravel, NextJS, Java & more",
             projects: () => "Projects listed: \none \n two \nthree \nfour",
+            clear: () => {
+                const elements = document.getElementsByClassName('line');
+                while (elements.length > 0) {
+                    elements[0].parentNode?.removeChild(elements[0]);
+                }
+                setHistory([]);
+                return '';
+            },
         };
+
+        commands.help = () => {
+            const commandList = Object.keys(commands).join(', \n');
+
+            return `Available commands: \n${commandList}`;
+        }
+
         return commands[command] ? commands[command]() : "Command not found";
     };
 
     const onEnter = () => {
-        handleCommand(input);
-        setInput('');
+        if (!isTyping) {
+            handleCommand(input);
+            setInput('');
+        }
     };
 
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            // Focus on the input field when any key is pressed
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+        };
+    });
+
     return (
-        <div className="terminal">
-            {history.map((item, index) => (
-                    <div key={index}>
+        <div className="font-dm">
+            <div className="flex justify-center items-start pt-3 text-[#f2f2f2]">
+                <p>🐉 drogaz.nl: ~/portfolio/terminal/v1</p>
+            </div>
+            <div className="p-10">
+
+                <div className="text text-[#56bdc9] font-bold pr-2">
+                    <p>Welcome to my portfolio, type 'help' to get started</p>
+                    <p>made around 24-8-2024</p>
+                </div>
+
+                {history.map((item, index) => (
+                    <div key={index} className="line space-y-.5 text-[#999999]">
                         <span>{item.command}</span>
-                        <div>{item.result}</div>
-                        </div>
+                        <div>{index === history.length - 1 ? displayedResult : item.result}</div>
+                    </div>
                 ))}
-            <div className="input-line">
-        <span>$</span>
-        <input
-    value={input}
-    onChange={(e) => setInput(e.target.value)}
-    onKeyDown={(e) => e.key === 'Enter' && onEnter()}
-    autoFocus
-    />
-    </div>
-    </div>
-);
-}
+                <div className="pt-2">
+                    <span className="text-[#97c278] pr-2 font-extrabold">→</span>
+                    <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && onEnter()}
+                        ref={inputRef}
+                        autoFocus={true}
+                        className="bg-transparent focus:outline-none"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Terminal;
